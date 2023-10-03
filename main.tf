55a6b823e14c33a99f64e728545849a550386f18
Jakob Boghdady <58260820+jakoberpf@users.noreply.github.com>
2023-10-03 21:51:55 +0200

    add provider versions

--- main.tf
+++ main.tf
@@ -1,13 +1,16 @@
 terraform {
   required_providers {
     kubernetes = {
-      source = "hashicorp/kubernetes"
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.23.0"
     }
     kubectl = {
-      source = "gavinbunney/kubectl"
+      source  = "gavinbunney/kubectl"
+      version = "~> 1.14.0"
     }
     helm = {
-      source = "hashicorp/helm"
+      source  = "hashicorp/helm"
+      version = "~> 2.11.0"
     }
   }
 }
